42b3776b256630891e59d66f209cd8367944dd66
Helge Heß <devteam@zeezide.de>
2019-01-17 12:17:49 +0100

    Add a Swift 4 Package.swift
    Because ... 🤦‍♀️

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,12 +1,9 @@
 // swift-tools-version:4.0
-
 import PackageDescription
 
 let package = Package(
-  name: "cows",
-  products: [
-    .library(name: "cows", targets: ["cows"]),
-  ],
-  dependencies: [],
-  targets: [ .target(name:"cows") ]
+  name         : "cows",
+  products     : [ .library(name: "cows", targets: ["cows"]) ],
+  dependencies : [],
+  targets      : [ .target(name:"cows") ]
 )
