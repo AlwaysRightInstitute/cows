8346273a78e5a15b2f065b321cf1efb2f9a3a4d9
Helge Heß <devteam@zeezide.de>
2024-08-17 15:40:20 +0200

    Drop unused tests
    ...

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
     .executable(name: "vaca", targets: [ "vaca" ])
   ],
   dependencies: [
-    .package(url: "https://source.skip.tools/skip.git", from: "1.0.4"),
+    .package(url: "https://source.skip.tools/skip.git",     from: "1.0.4"),
     .package(url: "https://source.skip.tools/skip-lib.git", from: "1.0.0")
   ],
   targets      : [ 
-    .target(name: "cows", dependencies: [.product(name: "SkipLib", package: "skip-lib")],
-            plugins: [ .plugin(name: "skipstone", package: "skip") ]),
-    .testTarget(name: "cowsTests", dependencies: ["cows", .product(name: "SkipTest", package: "skip")],
-                plugins: [ .plugin(name: "skipstone", package: "skip") ]),
+    .target(name         : "cows", 
+            dependencies : [ .product(name: "SkipLib",   package: "skip-lib") ],
+            plugins      : [ .plugin (name: "skipstone", package: "skip") ]),
     .executableTarget(name: "vaca", dependencies: [ "cows" ])
   ]
 )
